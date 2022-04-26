275409ba836f69a491d319df073b6f8a83e618d3
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-04-25 21:58:00 -0300

    pokedex con pokeapi v 0.2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <h3 className='copy'>Dessarrollado por &copy;Nicolas Oviedo </h3>
   </React.StrictMode>,
   document.getElementById('root')
 );
